ac0c6b49b26844650cf2cb63db555920707481e7
saraval09 <saramaneval@Saras-MacBook-Pro.local>
2016-05-23 00:01:08 -0400

    Grocery app
    API connection

---
+++ Grocery_List_App/js/auth.js
@@ -0,0 +1,9 @@
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '1086111638101440', // your App ID
+        'clientSecret'  : '25d34bb385884253f50d5ac7f3791ae5', // your App Secret
+        'callbackURL'   : 'http://localhost:3630/auth/facebook/callback'}
+
+};
